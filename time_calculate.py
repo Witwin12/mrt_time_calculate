4f7620302205593e5c6e1ebd7961d8cca9cbe393
Witwin12 <kunwin12@gmail.com>
2025-10-21 20:30:18 +0700

    fix time shift for weekdays

--- time_calculate.py
+++ time_calculate.py
@@ -1,102 +1,168 @@
 from datetime import time,datetime,timedelta
 
+from datetime import time, timedelta
+
 stations = {
     'pp1': {
         'up': {'start': time(5, 30, 0), 'end': time(22, 56, 0)},
-        'down': {'start': time(6, 32, 0), 'end': time(23, 8, 0)}
+        'down': {'start': timedelta(minutes=32), 'end': time(0, 8, 0)}
     },
     'pp2': {
-        'up': {'start': time(5, 32, 0), 'end': time(22, 58, 0)},
-        'down': {'start': time(6, 30, 0), 'end': time(0, 6, 0)}
+        'up': {'start': timedelta(minutes=2), 'end': time(22, 58, 0)},
+        'down': {'start': timedelta(minutes=30), 'end': time(0, 6, 0)}
     },
     'pp3': {
-        'up': {'start': time(5, 35, 0), 'end': time(23, 1, 0)},
-        'down': {'start': time(6, 28, 0), 'end': time(0, 3, 0)}
+        'up': {'start': timedelta(minutes=5), 'end': time(23, 1, 0)},
+        'down': {'start': timedelta(minutes=28), 'end': time(0, 3, 0)}
     },
     'pp4': {
-        'up': {'start': time(5, 37, 0), 'end': time(23, 3, 0)},
-        'down': {'start': time(6, 25, 0), 'end': time(0, 1, 0)}
+        'up': {'start': timedelta(minutes=7), 'end': time(23, 3, 0)},
+        'down': {'start': timedelta(minutes=25), 'end': time(0, 1, 0)}
     },
     'pp5': {
-        'up': {'start': time(5, 39, 0), 'end': time(23, 5, 0)},
-        'down': {'start': time(6, 23, 0), 'end': time(23, 59, 0)}
+        'up': {'start': timedelta(minutes=9), 'end': time(23, 5, 0)},
+        'down': {'start': timedelta(minutes=23), 'end': time(23, 59, 0)}
     },
     'pp6': {
-        'up': {'start': time(5, 42, 0), 'end': time(23, 7, 0)},
-        'down': {'start': time(6, 21, 0), 'end': time(23, 57, 0)}
+        'up': {'start': timedelta(minutes=12), 'end': time(23, 7, 0)},
+        'down': {'start': timedelta(minutes=21), 'end': time(23, 57, 0)}
     },
     'pp7': {
-        'up': {'start': time(5, 44, 0), 'end': time(23, 9, 0)},
-        'down': {'start': time(6, 20, 0), 'end': time(23, 55, 0)}
+        'up': {'start': timedelta(minutes=14), 'end': time(23, 9, 0)},
+        'down': {'start': timedelta(minutes=20), 'end': time(23, 55, 0)}
     },
     'pp8': {
-        'up': {'start': time(5, 46, 0), 'end': time(23, 12, 0)},
-        'down': {'start': time(6, 17, 0), 'end': time(23, 52, 0)}
+        'up': {'start': timedelta(minutes=16), 'end': time(23, 12, 0)},
+        'down': {'start': timedelta(minutes=17), 'end': time(23, 52, 0)}
     },
     'pp9': {
-        'up': {'start': time(5, 49, 0), 'end': time(23, 14, 0)},
-        'down': {'start': time(6, 15, 0), 'end': time(23, 50, 0)}
+        'up': {'start': timedelta(minutes=19), 'end': time(23, 14, 0)},
+        'down': {'start': timedelta(minutes=15), 'end': time(23, 50, 0)}
     },
     'pp10': {
-        'up': {'start': time(5, 51, 0), 'end': time(23, 16, 0)},
-        'down': {'start': time(6, 13, 0), 'end': time(23, 48, 0)}
+        'up': {'start': timedelta(minutes=21), 'end': time(23, 16, 0)},
+        'down': {'start': timedelta(minutes=13), 'end': time(23, 48, 0)}
     },
     'pp11': {
-        'up': {'start': time(5, 53, 0), 'end': time(23, 18, 0)},
-        'down': {'start': time(6, 11, 0), 'end': time(23, 46, 0)}
+        'up': {'start': timedelta(minutes=23), 'end': time(23, 18, 0)},
+        'down': {'start': timedelta(minutes=11), 'end': time(23, 46, 0)}
     },
     'pp12': {
-        'up': {'start': time(5, 57, 0), 'end': time(23, 21, 0)},
-        'down': {'start': time(6, 8, 0), 'end': time(23, 43, 0)}
+        'up': {'start': timedelta(minutes=27), 'end': time(23, 21, 0)},
+        'down': {'start': timedelta(minutes=8), 'end': time(23, 43, 0)}
     },
     'pp13': {
-        'up': {'start': time(5, 59, 0), 'end': time(23, 24, 0)},
-        'down': {'start': time(6, 6, 0), 'end': time(23, 40, 0)}
+        'up': {'start': timedelta(minutes=29), 'end': time(23, 24, 0)},
+        'down': {'start': timedelta(minutes=6), 'end': time(23, 40, 0)}
     },
     'pp14': {
-        'up': {'start': time(6, 2, 0), 'end': time(23, 26, 0)},
-        'down': {'start': time(6, 4, 0), 'end': time(23, 38, 0)}
+        'up': {'start': timedelta(minutes=32), 'end': time(23, 26, 0)},
+        'down': {'start': timedelta(minutes=4), 'end': time(23, 38, 0)}
     },
     'pp15': {
-        'up': {'start': time(6, 5, 0), 'end': time(23, 29, 0)},
-        'down': {'start': time(6, 2, 0), 'end': time(23, 35, 0)}
+        'up': {'start': timedelta(minutes=35), 'end': time(23, 29, 0)},
+        'down': {'start': timedelta(minutes=2), 'end': time(23, 35, 0)}
     },
     'pp16': {
-        'up': {'start': time(6, 7, 0), 'end': time(23, 31, 0)},
+        'up': {'start': timedelta(minutes=37), 'end': time(23, 31, 0)},
         'down': {'start': time(6, 0, 0), 'end': time(23, 33, 0)}
     }
 }
+
 # แสดงเวลาปัจจุบัน
 real_time_now = datetime.now()
 today = datetime.now().date()  # เอาวันปัจจุบันมาใช้ใน combine()
 next_train = []
-
+time_first_station = []
 def weekends_time(station,direction):
      
     ### คำนวณเวลาสำหรับวันหยุด เสา-อาทิตย์
     if direction =="up":
-        new_time_start = datetime.combine(today,stations[f"pp{station}"]['up']['start']) 
-        new_time_end = datetime.combine(today,stations[f"pp{station}"]['up']['end'])
-        time_calculate = new_time_start + timedelta(minutes=30)
+        time_start = datetime.combine(today,stations[f"pp{station}"]['up']['start']) 
+        time_end = datetime.combine(today,stations[f"pp{station}"]['up']['end'])
+        
     elif direction =="down":
-        new_time_start = datetime.combine(today,stations[f"pp{station}"]['down']['start']) 
-        new_time_end = datetime.combine(today,stations[f"pp{station}"]['down']['end'])
-        time_calculate = new_time_start + timedelta(minutes=18)
-    if new_time_end < new_time_start:
-        new_time_end += timedelta(days=1)
-    print(f"ตารางเวลาของสถานี:pp{station}")
-    while time_calculate < new_time_end:
-        print(time_calculate.strftime(('%H:%M:%S')))
-        time_calculate += timedelta(minutes=9,seconds=30)
+        time_start = datetime.combine(today,stations[f"pp{station}"]['down']['start']) 
+        time_end = datetime.combine(today,stations[f"pp{station}"]['down']['end'])
 
-        check_status_train(time_calculate)
-    print(f"ขบวนสุดท้ายออกเวลา: {new_time_end.strftime(('%H:%M:%S'))}")
+    print(f"ตารางเวลาของสถานี:pp{station}")
+    for t in time_first_station:
+        t += time_start
+        print(t.strftime(('%H:%M:%S')))
+        t += timedelta(minutes=9,seconds=30)
+
+        check_status_train(t)
+    print(f"ขบวนสุดท้ายออกเวลา: {t.strftime(('%H:%M:%S'))}")
     if next_train == []:
         print("ไม่มีขบวนถัดไป")
     else:
         print(f"ขบวนถัดไปมา{next_train[0]}")
 
 def weekdays_time(station,direction):
+    #คำนวณเวลาสำหรับวันทำงาน จันทร์ - ศุกร์
+    if direction =="up":
+        new_time_start = (stations[f"pp{station}"]['up']['start']) 
+        new_time_end = datetime.combine(today,stations[f"pp{station}"]['up']['end'])
+    elif direction =="down":
+        new_time_start = (stations[f"pp{station}"]['down']['start']) 
+        new_time_end = datetime.combine(today,stations[f"pp{station}"]['down']['end'])
+
+    print(f"ตารางเวลาของสถานี:pp{station}")
+    for t in time_first_station:
+        t_time = datetime.strptime(t, "%H:%M:%S").time()
+        t_datetime = datetime.combine(today, t_time)
+        # แล้วบวกกับ new_time_start (ต้องเป็น timedelta)
+        new_time = t_datetime + new_time_start  # ถ้า new_time_start เป็น timedelta
+        print(new_time.strftime(('%H:%M:%S')))
+
+        check_status_train(new_time)
+    print(f"ขบวนสุดท้ายออกเวลา: {new_time.strftime(('%H:%M:%S'))}")
+    if next_train == []:
+        print("ไม่มีขบวนถัดไป")
+    else:
+        print(f"ขบวนถัดไปมา{next_train[0]}")
+
+def check_status_train(time_calculate):
+    # check next train alive
+    if real_time_now < time_calculate:
+        next_train.append(time_calculate.strftime(('%H:%M:%S')))
+
+def station_ask():
+    #Check station
+    station = input("Where are you live now?")
+    direction = input("Which directions you want to go?")
+    if (station == "16" and direction =='up') or (station == "1" and direction =='down'):
+        print("This is terminal station")
+                # คืนค่า list จากฟังก์ชันเวลา (วันธรรมดาหรือวันหยุด)
+    elif (station == "16" and direction =='down') or (station == "1" and direction =='up'):
+            # คืนค่า list จากฟังก์ชันเวลา (วันธรรมดาหรือวันหยุด)
+        if real_time_now.weekday() < 5:
+            return first_station_weekdays_time(direction),print(time_first_station)
+        else:
+            return first_station_weekend_time(direction), print(time_first_station)
+    elif real_time_now.weekday() < 5:
+        return(first_station_weekdays_time(direction),weekdays_time(station,direction))
+    else:
+        return(first_station_weekend_time(direction),weekends_time(station,direction))
+    
+    
+def first_station_weekend_time(direction):
+    ### คำนวณเวลาสำหรับสถานีแรกวันหยุด เสา-อาทิตย์
+    if direction =="up":
+        new_time_start = datetime.combine(today,stations["pp1"]['up']['start']) 
+        new_time_end = datetime.combine(today,stations["pp1"]['up']['end'])
+        time_calculate = new_time_start + timedelta(minutes=30)
+    elif direction =="down":
+        new_time_start = datetime.combine(today,stations["pp16"]['down']['start']) 
+        new_time_end = datetime.combine(today,stations["pp16"]['down']['end'])
+        time_calculate = new_time_start + timedelta(minutes=18)
+
+    while time_calculate < new_time_end:
+        time_first_station.append(time_calculate.strftime(('%H:%M:%S')))
+        time_calculate += timedelta(minutes=9,seconds=30)
+    time_first_station.append(time_calculate.strftime(('%H:%M:%S')))
+
+def first_station_weekdays_time(direction):
     #คำนวณเวลาสำหรับวันทำงาน จันทร์ - ศุกร์
     time_in_5am_to_6am = time(6,30,0)
     time_in_6am_to_8am = (time(8,30,0))
@@ -106,17 +172,14 @@ def weekdays_time(station,direction):
     time_in_8pm_to_9pm = (time(21,0,0))
     time_in_9pm_to_0am = (time(23,59,59))
     if direction =="up":
-        new_time_start = datetime.combine(today,stations[f"pp{station}"]['up']['start']) 
-        new_time_end = datetime.combine(today,stations[f"pp{station}"]['up']['end'])
+        new_time_start = datetime.combine(today,stations["pp1"]['up']['start']) 
+        new_time_end = datetime.combine(today,stations["pp1"]['up']['end'])
     elif direction =="down":
-        new_time_start = datetime.combine(today,stations[f"pp{station}"]['down']['start']) 
-        new_time_end = datetime.combine(today,stations[f"pp{station}"]['down']['end'])
-    if new_time_end < new_time_start:
-        new_time_end += timedelta(days=1)
-    print(f"ตารางเวลาของสถานี:pp{station}")
-    while new_time_start < new_time_end:
+        new_time_start = datetime.combine(today,stations["pp16"]['down']['start']) 
+        new_time_end = datetime.combine(today,stations["pp16"]['down']['end'])
 
-        print(new_time_start.strftime(('%H:%M:%S')))
+    time_first_station.append(new_time_start.strftime(('%H:%M:%S')))#add first time
+    while new_time_start < new_time_end:
         if  new_time_start.time() < time_in_5am_to_6am:
             new_time_start += timedelta(minutes=7,seconds=12)
         elif new_time_start.time() < time_in_6am_to_8am:
@@ -131,29 +194,10 @@ def weekdays_time(station,direction):
             new_time_start += timedelta(minutes=6, seconds=25)
         elif new_time_start.time() < time_in_9pm_to_0am:
             new_time_start += timedelta(minutes=9, seconds=30)
+        time_first_station.append(new_time_start.strftime(('%H:%M:%S')))
 
-        check_status_train(new_time_start)
-    print(f"ขบวนสุดท้ายออกเวลา: {new_time_end.strftime(('%H:%M:%S'))}")
-    if next_train == []:
-        print("ไม่มีขบวนถัดไป")
-    else:
-        print(f"ขบวนถัดไปมา{next_train[0]}")
+       
+station_ask()
 
-def check_status_train(time_calculate):
-    # check next train alive
-    if real_time_now < time_calculate:
-        next_train.append(time_calculate.strftime(('%H:%M:%S')))
 
-def station_ask(station,direction):
-    #Check station
-    if station == "16" and direction =='up':
-        print("This is terminal station")
-    elif station == "1" and direction =='down':
-        print("This is terminal station")
-    elif real_time_now.weekday() < 5:
-        return(weekdays_time(station,direction))
-    else:
-        return(weekends_time(station,direction))
-    
 
-station_ask(input("where are you now??"),input("which directions?"))
